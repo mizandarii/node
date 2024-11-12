fc8831882117e8901ad154a11983a18f0b307f0f
mizandarii <kristiina.parri@ivkhk.ee>
2024-11-12 14:26:33 +0200

    path +comments

--- refs/path_ref.js
+++ refs/path_ref.js
@@ -1,11 +1,22 @@
-const path = require('path')
+const path = require('path');
 
-console.log(__filename)
-console.log(path.basename(__filename))
-console.log(path.dirname(__filename))
-console.log(path.extname(__filename))
+// вывод полного пути и имени текущего файла
+console.log(__filename); 
 
-console.log(path.parse(__filename))
+// вывод названия файла (последней части пути)
+console.log(path.basename(__filename)); 
 
-console.log(path.join(__dirname, 'test', 'second.html'))
-console.log(path.resolve(__dirname, './test', '/second.html'))
+// вывод директории (пути) к файлу без самого имени файла
+console.log(path.dirname(__filename)); 
+
+// вывод расширения файла (например, .js, .txt)
+console.log(path.extname(__filename)); 
+
+// парсинг пути в объект, который содержит различные части пути
+console.log(path.parse(__filename)); 
+
+// соединение частей пути в один путь (получится путь к файлу second.html внутри папки test)
+console.log(path.join(__dirname, 'test', 'second.html')); 
+
+// формирование абсолютного пути с учётом текущего каталога (__dirname)
+console.log(path.resolve(__dirname, './test', '/second.html')); 
