1884cebf424b627722ece21bbc85c0f366d5af54
mizandarii <kristiina.parri@ivkhk.ee>
2024-11-12 13:15:05 +0200

    path

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+//импорт объекта юзер и метода
 const obj = require('./user')
 
+//вывод объекта в консоль
 console.log(obj.user)
 
+//вызов метода (печатает hello  в консоль)
 obj.sayHello()
 
-
+//вывод рабочей директории и файла
 console.log("work dir", __dirname)
 console.log("work file", __filename)
--- user.js
+++ user.js
@@ -1,8 +1,10 @@
+//создаем объект пользователь
 const user = {
     name:'Vasya',
     age: 30
 }
 
+//экспортируем объект и метод, выводящий в консоль приветствие
 module.exports = {
     user:user,
     sayHello(){
