45583c5608d636f1c38c52a3595ba7fefba3339e
mizandarii <kristiina.parri@ivkhk.ee>
2024-11-13 08:54:01 +0200

    6.events

--- refs/events_ref.js
+++ refs/events_ref.js
@@ -1,18 +1,37 @@
 const EventEmitter = require('events')
 
+// class Logger extends EventEmitter{
+//     log(message){
+//         this.emit('message', `${message} ${Date.now()}`)
+//     }
+// }
+
+// const logger = new Logger()
+
+// logger.on('message', data => [
+//     console.log(data)
+// ])
+
+// logger.log('Hello')
+// logger.log('Hello')
+// logger.log('Hello')
+
+let count = 1;
 class Logger extends EventEmitter{
-    log(message){
-        this.emit('message', `${message} ${Date.now()}`)
+    
+    log(myOwnMessage){
+        this.emit('myOwnMessage', `${count} ${myOwnMessage}`)
+        count ++;
     }
 }
 
 const logger = new Logger()
 
-logger.on('message', data => [
+logger.on('myOwnMessage', data => [
     console.log(data)
 ])
 
-logger.log('Hello')
-logger.log('Hello')
-logger.log('Hello')
+logger.log("A witch's vehicle goes brrroom brrroom!")
+logger.log('I failed math so many times at school, I can’t even count.')
+//logger.log('Third')
 
